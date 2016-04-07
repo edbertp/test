7f662a84422d166ea812eed4ccfe23a248a7dc29
Johann BICH <johannbich@gmail.com>
2016-04-07 17:24:10 +0800

    debug openshift

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8080, function () {
+app.listen(ip, port, function () {
   console.log('Example app listening on port 3000!');
 });
